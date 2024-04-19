94748dc7f81c8bad8361a9417cd2d32f48ea3979
kvpradeep-03 <kvpradeep60@gmail.com>
2024-04-19 06:49:58 +0000

    modified load file

--- libs/load.php
+++ libs/load.php
@@ -2,11 +2,15 @@
 
 function load_template($name){
  
-    //__DIR__ provides the absolute path to the directory containing the current PHP script
+    //__DIR__ provides the absolute path to the directory containing the current PHP script, tightly coupled with current working directory
     //Current dir is libs (htdocs/photogram/app/libs) => One step back (htdocs/photogram/app)->(Out of libs dir, Then it currently inside the app dir) => app 
     //[*changes the dir not folder*]
-    include __DIR__."/../_templates/$name.php";
-        
+    //include __DIR__."/../_templates/$name.php";
+    
+    //superglobal is an array containing information such as headers, paths, and script locations, tightly coupled with server envirnoment and request details. 
+    //these superglobal are responsible for transferring information(input request) from apache to php.
+    //$_SERVER[DOCUMENT_ROOT] => /var/www/html 
+    include $_SERVER['DOCUMENT_ROOT']."/photogram/app/_templates/$name.php";
      
 }
 ?>
