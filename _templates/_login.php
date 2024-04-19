8eef267e9d9776aa10f0e0378a3fc52ea7bd9138
kvpradeep-03 <kvpradeep60@gmail.com>
2024-04-19 19:50:47 +0000

    created login form

---
+++ _templates/_login.php
@@ -0,0 +1,49 @@
+<?php
+
+$username = $_POST['email'];
+$password = $_POST['password'];
+
+$result = validate_credential($username, $password);
+
+if($result){
+    ?>
+    <main class="container">
+        <div class="bg-body-tertiary p-5 rounded mt-3">
+            <h1>Login Success</h1>
+            <p class="lead">Now you can explore the trending clicks!.</p>
+            <a class="btn btn-lg btn-primary" href="/docs/5.3/components/navbar/" role="button">View navbar docs »</a>
+        </div>
+    </main>
+    <?
+} else {
+
+?>
+
+<main class="form-signin w-100 m-auto">
+  <form action="login.php" method="post">
+    <img class="mb-4" src="https://cdn-icons-png.flaticon.com/128/10401/10401230.png" alt="" width="100" height="87">
+    <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+
+    <div class="form-floating">
+      <input name = "email" type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+      <label for="floatingInput">Email address</label>
+    </div>
+    <div class="form-floating">
+      <input name = "password" type="password" class="form-control" id="floatingPassword" placeholder="Password">
+      <label for="floatingPassword">Password</label>
+    </div>
+
+    <div class="form-check text-start my-3">
+      <input class="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault">
+      <label class="form-check-label" for="flexCheckDefault">
+        Remember me
+      </label>
+    </div>
+    <button class="btn btn-primary w-100 py-2 hvr-wobble-vertical" type="submit">Sign in</button>
+    <p class="mt-5 mb-3 text-body-secondary">© 2017–2024</p>
+  </form>
+</main>
+
+<?php
+}
+?>
