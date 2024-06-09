3afbaea4f5d8410eb082759808196410a232439d
kvpradeep-03 <kvpradeep60@gmail.com>
2024-06-09 19:09:45 +0000

    else issue can't able to fix.

--- _templates/_signup.php
+++ _templates/_signup.php
@@ -1,22 +1,57 @@
+<?php
+    $signup = false;
+    if(isset($_POST['username']) and isset($_POST['phone']) and isset($_POST['email']) and isset($_POST['password'])){
+      $username = $_POST['username'];
+      $password = $_POST['password'];
+      $email = $_POST['email'];
+      $phone = $_POST['phone'];
+      $error = signup($username,$password,$email,$phone);
+      $signup = true;
+    }
+?>
+
+<?php
+    if($signup){
+      if(!$error){
+?>
+        <main class="container">
+          <div class="bg-body-tertiary p-5 rounded mt-3">
+            <h1>Signup Success</h1>
+            <p class="lead">Now you can login <a href="/photogram/app/login.php">here</a></p>
+          </div>
+        </main>
+<?php
+    } else {
+?>
+        <main class="container">
+          <div class="bg-body-tertiary p-5 rounded mt-3">
+            <h1>Signup Fail</h1>
+            <p class="lead">Something went wrong, <?=$error?></p>
+          </div>
+        </main>
+<?php
+    } 
+  } else {
+?>
 <!--signup form-->
 <main class="form-signup w-100 m-auto">
-  <form action="test.php" method="post">
+  <form action="signup.php" method="post">
     <img class="mb-4" src="https://cdn-icons-png.flaticon.com/128/10401/10401230.png" alt="" width="100" height="87">
     <h1 class="h3 mb-3 fw-normal">Please sign up</h1>
     <div class="form-floating">
-      <input name = "username" type="text" class="form-control" id="floatingInput" placeholder="username">
+      <input name="username" type="text" class="form-control" id="floatingInput" placeholder="username">
       <label for="floatingInputusername">Username</label>
     </div>
     <div class="form-floating">
-      <input name = "phone" type="text" class="form-control" id="floatingInput" placeholder="phone">
+      <input name="phone" type="text" class="form-control" id="floatingInput" placeholder="phone">
       <label for="floatingInputphone">Phone</label>
     </div>
     <div class="form-floating">
-      <input name = "email" type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+      <input name="email" type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
       <label for="floatingInputemail">Email address</label>
     </div>
     <div class="form-floating">
-      <input name = "password" type="password" class="form-control" id="floatingPassword" placeholder="Password">
+      <input name="password" type="password" class="form-control" id="floatingPassword" placeholder="Password">
       <label for="floatingPassword">Password</label>
     </div>
 
@@ -30,3 +65,7 @@
     <p class="mt-5 mb-3 text-body-secondary">© 2017–2024</p>
   </form>
 </main>
+
+<?php
+  }
+?>
