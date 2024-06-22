e6e1d60d4de34bd50e223763939621217a4be0dd
kvpradeep-03 <kvpradeep60@gmail.com>
2024-06-22 08:26:43 +0000

    created session class file

---
+++ libs/includes/Session.class.php
@@ -0,0 +1,38 @@
+<?php
+
+class Session{
+    
+    public static function start(){
+        session_start();    //starts the session.
+    }
+
+    public static function unset(){
+        session_unset();    //unsets the session.
+    }
+
+    public static function destroy(){
+        session_destroy();  //destroys the current session.
+    }
+
+    public static function set($key,$value){    
+        $_SESSION[$key] = $value;   //[$key]: This accesses the specific session variable by the key provided. $value;: This assigns the value to the session variable.
+    }
+
+    public static function delete($key){
+        unset($_SESSION[$key]); //delets the session key.
+    }
+
+    public static function isset($key){
+        return isset($_SESSION[$key]);  //checks whether the session key is exist or not.
+    }
+
+    public static function get($key,$default = false){
+        if(Session::isset($key)){
+            return $_SESSION[$key];  //returns the key.
+        }else{
+            return $default;
+        }
+    }
+}
+
+?>
