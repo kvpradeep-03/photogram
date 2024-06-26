64d48722a643e88cf1f1c90c9a2bb3d8d101a795
kvpradeep-03 <kvpradeep60@gmail.com>
2024-06-26 10:14:02 +0000

    comments added

--- libs/includes/Database.class.php
+++ libs/includes/Database.class.php
@@ -1,27 +1,27 @@
 <?php
  
  class Database{
-    public static $conn = null;
+    public static $conn = null; //checkes the connection is empty?
     public static function getConnection(){
-        if(Database::$conn == null){
+        if(Database::$conn == null){    //at initial new connection the $conn ->obeviously empty
             $servername = "mysql.selfmade.ninja";
             $username = "photogram_db";
             $password = "prad2003";
             $dbname = "photogram_db_auth";
 
             // Create connection
-            $connection = new mysqli($servername, $username, $password, $dbname);
+            //$connection -> An instance of the mysqli class, which is used to connect to a MySQL database.
+            $connection = new mysqli($servername, $username, $password, $dbname);    
             // Check connection
-            if ($connection->connect_error) {
+            //->connect_error -> A property of the mysqli object.If the connection attempt fails,this property contains a string describing the error that occurred.
+            if ($connection->connect_error) {   
             die("Connection failed: " . $connection->connect_error);
             }else{
-                echo "connection established.";
-                Database::$conn = $connection;  //replacing null with actual connection
+                Database::$conn = $connection;  //replacing null with actual connection (server connection information status)
                 return Database::$conn;
             }   
 
         }else{
-            echo "Re establishing the connection .";
             return Database::$conn;
         }
     }
