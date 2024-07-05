1ec17f27ceff88a75d3f4e8257f9846871b3e84c
kvpradeep-03 <kvpradeep60@gmail.com>
2024-07-05 17:08:11 +0000

    passwordhashed and verified in login and signup form

--- libs/includes/User.class.php
+++ libs/includes/User.class.php
@@ -1,10 +1,17 @@
 <?php
 
 class User{
+   
     private $conn;
+    //private static $salt = "bfqdq%ijspiq@vnk!b&fds*dca#vafva";  //cryptographic hashing -> a salt is a random value added to a password before hashing it. The purpose of using a salt is to enhance the security
+    
     public static function signup($user,$pass,$email,$phone){
-        $pass = md5($pass); //hashes the password while saving in db
         $conn = Database::getconnection();  //connection is fetches from Database.class.php
+        //$pass = md5($pass).User::$salt; //hashes the password while saving in db
+        $options = [
+            'cost' => 9,
+        ];
+        $pass = password_hash($pass,PASSWORD_BCRYPT,$options);
         $sql = "INSERT INTO `auth` (`username`, `password`, `email`, `phone`, `block`, `active`)    
         VALUES ('$user', '$pass', '$email', '$phone', '0', '1')";   //inserting the data's into sql table
         $error = false;
@@ -29,13 +36,13 @@ class User{
     }
 
     public static function login($user,$pass){
-        $pass = md5($pass); //while the $pass var fetches this line it hashes into(32 bit char) after hashing even we can know what's the real pass is.
-        $query = "SELECT * FROM `auth` WHERE `username` = '$user'"; //selects username from db
         $conn = Database::getConnection();
+        $query = "SELECT * FROM `auth` WHERE `username` = '$user'"; //selects username from db
         $result = $conn->query($query);
         if($result->num_rows == 1){ // Checks if the result set contains any rows
             $row = $result->fetch_assoc();  //fetch_assoc() ->returns the datas in the form of associative array.
-            if($row['password'] == $pass){
+            //if($row['password'] == $pass){
+            if(password_verify($pass,$row['password'])){
                 return $row;    //returns the entire particular row.
             }else{
                 return false;
