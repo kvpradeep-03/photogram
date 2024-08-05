33e7da61cfe1cf070d3a1546f0242d61a4db7dc7
kvpradeep-03 <kvpradeep60@gmail.com>
2024-08-05 19:09:12 +0000

    userObj logic builded

--- libs/includes/User.class.php
+++ libs/includes/User.class.php
@@ -2,18 +2,35 @@
 
 class User{
    
-    private $conn;
-    //private static $salt = "bfqdq%ijspiq@vnk!b&fds*dca#vafva";  //cryptographic hashing -> a salt is a random value added to a password before hashing it. The purpose of using a salt is to enhance the security
+    public $conn;
+    public $username; // Declare the username property
+    public $id; // Declare the id property   
+    
+    //_call fun will be called if the called function(method) is not avalilabe in the class(like switchcase default in py)
+    public function __call($name,$arguments){  
+
+        //Removes non-alphanumeric characters from the substring starting at the 4th character of $name.
+        //substr simply cuts a str into a represented index
+        $property = preg_replace("/[^0-9a-zA-Z]/","",substr($name, 3)); 
+        //Converts camelCase to snake_case by inserting an underscore before capital letters.
+        $property = strtolower(preg_replace('/\B([A-Z])/','_$1',$property));    
+        if(substr($name,0,3) == 'get'){ 
+            return $this->_get_data($property);
+        }elseif(substr($name,0,3) == 'set'){
+            return $this->_set_data($name,$arguments[0]);
+            
+        }
+    }
     
     public static function signup($user,$pass,$email,$phone){
-        $conn = Database::getconnection();  //connection is fetches from Database.class.php
-        //$pass = md5($pass).User::$salt; //hashes the password while saving in db
+        //connection is fetches from Database.class.php
+        $conn = Database::getconnection();  
         $options = [
             'cost' => 9,
         ];
         $pass = password_hash($pass,PASSWORD_BCRYPT,$options);
         $sql = "INSERT INTO `auth` (`username`, `password`, `email`, `phone`, `block`, `active`)    
-        VALUES ('$user', '$pass', '$email', '$phone', '0', '1')";   //inserting the data's into sql table
+        VALUES ('$user', '$pass', '$email', '$phone', '0', '1')";  
         $error = false;
         try {
             //checks inserted query status(true/false)
@@ -30,20 +47,20 @@ class User{
         }
     
         // $conn->close();
-        return $error; //returns true/false (redirects to _sighup.php)
+        //returns true/false (redirects to _sighup.php)
+        return $error; 
     
         
     }
 
     public static function login($user,$pass){
         $conn = Database::getConnection();
-        $query = "SELECT * FROM `auth` WHERE `username` = '$user'"; //selects username from db
+        $query = "SELECT * FROM `auth` WHERE `username` = '$user'"; 
         $result = $conn->query($query);
-        if($result->num_rows == 1){ // Checks if the result set contains any rows
-            $row = $result->fetch_assoc();  //fetch_assoc() ->returns the datas in the form of associative array.
-            //if($row['password'] == $pass){
+        if($result->num_rows == 1){     // Checks if the result set contains no.of rows == 1
+            $row = $result->fetch_assoc();      //fetch_assoc() ->returns the datas in the form of associative array. 
             if(password_verify($pass,$row['password'])){
-                return $row;    //returns the entire particular row.
+                return $row['username'];
             }else{
                 return false;
             }
@@ -54,41 +71,71 @@ class User{
 
     }
     
-    public function _construct($username){
+    //fetches the username by the initialization of signup object of User class at signup form
+    //_construct fetches id of loged user via their username else throws error.
+    public function __construct($username){
+       // print($username);  
         $this->conn = Database::getConnection();
-
-        $this->username = $username; 
-        
-        //TODO:write the code to fetch user data to fetch from the Database for the given username
-        $query = "SELECT `id` FROM `auth` WHERE `username` = $username";
-        $this->id = $conn->query($query); //update this from database
-        
-        
+        $this->username = $username;
+        $this->id = null;
+        $sql = "SELECT `id` FROM `auth` WHERE `username` = '$username' LIMIT 1";
+        $result = $this->conn->query($sql);
+        if($result->num_rows == 1){
+            $row = $result->fetch_assoc(); 
+            $this->id = $row['id']; //update this from database
+        }else{
+            throw new exception("Username Not Found :( ");
+        }
     }
 
-    public function authenticate(){
-
+    //this function  helps to retrive data from database
+    public function _get_data($var){
+        if(!$this->conn){
+            $this->conn = Database::getConnection();
+        }
+        $sql = "SELECT `$var` FROM `users` WHERE `id` = '$this->id'"; 
+        $result = $this->conn->query($sql);
+        if($result and $result->num_rows == 1){
+            return $result->fetch_assoc()["$var"];      //retrieves the specified key value
+        }else{
+            return null;
+        }
     }
 
-    public function setBio($bio){
-        //TODO: write UPDATE cmd to change new bio
-        $query = "UPDATE users SET bio = 'bio changed successfully' WHERE id = '194'";
+    //this function  helps to set data in the database
+    public function _set_data($var, $data){
 
+        if(!$this->conn){
+            $this->conn = Database::getConnection();
+        }
+        $sql = "UPDATE `users` SET '$var'='$data' WHERE `id`='$this->id'";  
+        if($this->conn->query($sql)){
+            return true; 
+        }else{
+            return false;
+        }
+
+    
+    }
+  
+    //overrided dob
+    public function setDob($year, $month, $day){
+        if(checkdate($month, $day, $year)){     
+            return $this->__set('dob',"$year-$month-$day");
+        }else{
+            return false;
+        }
     }
 
-    public function getBio(){
-        //TODO: write SELECT cmd to get the bio
-        $query = "SELECT `bio` FROM `users` WHERE `id` = '194' LIMIT 50";
-        
+    //overrided username
+    public function getUsername(){      
+        return $this->username;
     }
 
-    public function setAvatar($link){
-        $query = "SELECT `instagram` FROM `users` WHERE `id` = '194' LIMIT 50";
+    public function authenticate()
+    {
     }
-
-    public function getAvatar(){
-        $query = "SELECT `instagram`, `twitter`, `facebook` FROM `users` WHERE `id` = '194' LIMIT 50";
-    }
+    
 
 
 }
