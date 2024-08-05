2e9c62acf63334ae5d9172f4333da05d7e4f06f9
kvpradeep-03 <kvpradeep60@gmail.com>
2024-08-05 19:03:14 +0000

    session tested in login page

--- logintest.php
+++ logintest.php
@@ -1,31 +1,34 @@
 <?php
 include 'libs/load.php';
-
-$user = isset($_GET['user'])?$_GET['user'] : '';
-$pass = isset($_GET['pass'])?$_GET['pass'] : '';
+$user = isset($_GET['user'])?$_GET['user'] : '';    //testuser
+$pass = isset($_GET['pass'])?$_GET['pass'] : '';    //passuser
 $result = null;
-$result = User::login($user,$pass); //fetches the row data here.
+$result = User::login($user,$pass); //fetches the username.
 
 if(isset($_GET['logout'])){
     Session::destroy();
     die("Session destroyed, <a href='logintest.php'>Login Again</a><br>");
 }
-if(Session::get('session_user')){//if true it store the [$result->($user,$pass)] to the $userdata var.
-    $userdata = Session::get('session_user');
-    print("Welcome back, $userdata[username]!<br>");
-    //$result = $userdata;
+
+if(Session::get('is_logedin')){   //retrives the previous session of user (session setted while logging in)  
+    $username = Session::get('session_username');
+    $userObj = new User($username); //if a session exists, use the session data to create the User object.
+    echo "Welcome back {$userObj->getUsername()} !<br>";
+
 }else{
     echo "No Session found, trying to login now...<br>";
     $result = User::login($user,$pass); //it again gets the user input for $user and $pass.
+    if($result){  
+        $userObj = new User($user);     //if session doesn't exist it create the User object using the newly obtained username and update the session
+        echo "Login success, $userObj->getUsername";
+        Session::set('is_logedin',true);  
+        Session::set('session_username',$result);
+    }else{
+        echo "login failed, $user<br>";
+
+    }
 }
-if($result){  
-    echo "login success $result[username] :)<br>";
-    //Session::set('is_logedin',true);    //sets login sessionid only if true
-    Session::set('session_user',$result);   //setting session_user as key variable and $result as user,pass,email,..(data in the form of associative array).
-    
-}else{
-    echo "login failed<br>";
-    
-    //echo $pass;
-}
+
+
+
 ?>
