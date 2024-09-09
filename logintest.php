0e9079a7da4ba0a4279907b81e5de7ecfbb5b973
kvpradeep-03 <kvpradeep60@gmail.com>
2024-09-09 19:41:48 +0000

    session tested for logging users

--- logintest.php
+++ logintest.php
@@ -10,23 +10,33 @@ if(isset($_GET['logout'])){
     die("Session destroyed, <a href='logintest.php'>Login Again</a><br>");
 }
 
-if(Session::get('is_logedin')){   //retrives the previous session of user (session setted while logging in)  
+/*
+[completed]
+1. check if session_token in php Session is available
+2. If yes, construct userSession and see if it successfull.
+3. check if the session is valid one.
+4. if valid print session vaidated
+5. else print session invalidate and ask user to login
+
+*/
+if(Session::get('session_token')){   //retrives the previous session of user (session setted while logging in)  
     $username = Session::get('session_username');
     $userObj = new User($username); //if a session exists, use the session data to create the User object.
-    echo "Welcome back {$userObj->getusername()} !<br>";
-    $userObj->setBio("i love tech ");
+    UserSession::authorize(Session::get('session_token'));
+    echo "Welcome back {$userObj->getUsername()}!<br>";
+    $userObj->setBio("i love tech <br>");
     echo "Bio changed Succesfully ... `{$userObj->getbio()}`<br>";
 
 }else{
-    echo "No Session found, trying to login now...<br>";
+    
     $result = User::login($user,$pass); //it again gets the user input for $user and $pass.
-    if($result){  
-        $userObj = new User($user);     //if session doesn't exist it create the User object using the newly obtained username and update the session
-        echo "Login success, $userObj->getUsername";
-        Session::set('is_logedin',true);  
-        Session::set('session_username',$result);
+    if($result){ 
+        $userObj = new User($result);     //if session doesn't exist it create the User object using the newly obtained username and update the session
+        echo "Login success, {$userObj->getUsername()}<br>";
+        Session::set('session_token',UserSession::authenticate($user,$pass));  
+        Session::set('session_username',$userObj->getUsername());
     }else{
-        echo "login failed, $user<br>";
+        echo "login failed, $user<br>"; //TODO: check the flow of login process
 
     }
 }
