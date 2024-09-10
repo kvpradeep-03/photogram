5d1f59209b9e6ec181d0af28f4839a6dd1392744
kvpradeep-03 <kvpradeep60@gmail.com>
2024-09-10 04:58:18 +0000

    commented php error display

--- libs/load.php
+++ libs/load.php
@@ -3,9 +3,9 @@
 //php error display
 
 // Enable error reporting for debugging purposes
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+//ini_set('display_errors', 1);
+//ini_set('display_startup_errors', 1);
+//error_reporting(E_ALL);
 
 
 //included class files
