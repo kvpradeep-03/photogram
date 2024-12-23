327f636a40fc66a54d0012fde92d56f4c0be8fd1
kvpradeep-03 <kvpradeep60@gmail.com>
2024-12-23 10:37:04 +0000

    config path issue

--- libs/includes/Database.class.php
+++ libs/includes/Database.class.php
@@ -14,18 +14,22 @@
 
             // Create connection
             //$connection -> An instance of the mysqli class, which is used to connect to a MySQL database.
-            $connection = new mysqli($servername, $username, $password, $dbname);    
-            // Check connection
-            //->connect_error -> A property of the mysqli object.If the connection attempt fails,this property contains a string describing the error that occurred.
-            if ($connection->connect_error) {   
-            die("Connection failed: " . $connection->connect_error);
-            }else{
-                //replacing null with actual connection (server connection information status)
-                Database::$conn = $connection; 
-                return Database::$conn;
-            }   
-
-        }else{
+            try {
+                $connection = new mysqli($servername, $username, $password, $dbname);    
+                // Check connection
+                //->connect_error -> A property of the mysqli object.If the connection attempt fails,this property contains a string describing the error that occurred.
+                if ($connection->connect_error) {   
+                    throw new Exception("Connection failed: " . $connection->connect_error);
+                } else {
+                    //replacing null with actual connection (server connection information status)
+                    Database::$conn = $connection; 
+                    return Database::$conn;
+                }
+            } catch (Exception $e) {
+                error_log($e->getMessage());
+                die("Database connection error. Please check the logs for more details.");
+            }
+        } else {
             return Database::$conn;
         }
     }
