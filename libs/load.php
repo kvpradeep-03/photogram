714ebfc198a8afcb66b15f4c776b3c9a3d25e319
kvpradeep-03 <kvpradeep60@gmail.com>
2024-12-23 09:49:26 +0000

    config path issue

--- libs/load.php
+++ libs/load.php
@@ -22,7 +22,7 @@ global $__site_config;
 // Determine the full path to 'photogramconfig.json'. 
 // If the document root is a symbolic link, resolve its actual path.
 // Otherwise, use the document root directly and append '/photogramconfig.json'.
-$__site_config_path = dirname(is_link($_SERVER['DOCUMENT_ROOT']) ? readlink($_SERVER['DOCUMENT_ROOT']) : $_SERVER['DOCUMENT_ROOT']) . '/photogramconfig.json';
+$__site_config_path = dirname(is_link($_SERVER['DOCUMENT_ROOT']) ? readlink($_SERVER['DOCUMENT_ROOT']) : $_SERVER['DOCUMENT_ROOT']) . '/var/photogram/photogramconfig.json';
 
 // Read the content of 'photogramconfig.json' from the resolved path 
 // and store it in the $__site_config variable.
