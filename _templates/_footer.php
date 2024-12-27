2a4a02eb78248a89e9216a73e418661d76a43908
kvpradeep-03 <kvpradeep60@gmail.com>
2024-12-27 20:29:55 +0000

    added signup link

--- _templates/_footer.php
+++ _templates/_footer.php
@@ -3,7 +3,7 @@
     <p class="float-end mb-1">
       <a href="#">Back to top</a>
     </p>
-    <p class="mb-1">Album example is &copy; Bootstrap, but please download and customize it for yourself!</p>
-    <p class="mb-0">New to Bootstrap? <a href="/">Visit the homepage</a> or read our <a href="../getting-started/introduction/">getting started guide</a>.</p>
+    <p class="mb-1">© 2017–2024 Company, Inc. · <a href="#">Privacy</a> · <a href="#">Terms</a></p>
+    <p class="mb-0">New to Photogram? <a href="<?=get_config('base_path')?>signup.php">Sign up</a></p>
   </div>
 </footer>
