a3280f469ae07b2640bf500b58870bbce1e7be56
kvpradeep-03 <kvpradeep60@gmail.com>
2024-12-30 13:31:04 +0000

    created webapi object

--- libs/load.php
+++ libs/load.php
@@ -15,20 +15,12 @@ include_once 'includes/Mic.class.php';
 include_once 'includes/User.class.php';
 include_once 'includes/Database.class.php';
 include_once 'includes/UserSession.class.php';
+include_once 'includes/WebAPI.class.php';
 
-//sets global var
-global $__site_config; 
 
-// Determine the full path to 'photogramconfig.json'. 
-// If the document root is a symbolic link, resolve its actual path.
-// Otherwise, use the document root directly and append '/photogramconfig.json'.
-$__site_config_path = dirname(is_link($_SERVER['DOCUMENT_ROOT']) ? readlink($_SERVER['DOCUMENT_ROOT']) : $_SERVER['DOCUMENT_ROOT']) . '/photogramconfig.json';
 
-// Read the content of 'photogramconfig.json' from the resolved path 
-// and store it in the $__site_config variable.
-$__site_config = file_get_contents($__site_config_path);
-
-Session::start();
+$wapi = new WebAPI();
+$wapi->initiateSession();
 
 function get_config($key, $default= null){
     //access global var
